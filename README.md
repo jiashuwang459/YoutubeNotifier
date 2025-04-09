7f60af4bcf72bc3f157b29fa8b48dddc144c1e6f
Jiashu Wang <jiashuwang459@gmail.com>
2025-04-08 22:54:35 -0700

    it works!

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
 # YoutubeNotifier
+
+
+
+# Prereqs
+
+* Python 3.x
+<!-- * DiscordPy -->
+* Google Sheets API
+
+
+## Discord Python API 
+
+### (Recommended) Setup a pyenv
+```bash
+python3 -m venv bot-env
+```
+#### To Activate:
+##### Linux:
+```bash
+source bot-env/bin/activate
+```
+##### Windows:
+```bash
+bot-env\Scripts\activate.bat
+```
+<!-- 
+### Install Discord Dependencies
+more info [here](https://discordpy.readthedocs.io/en/stable/intro.html)
+
+```bash
+pip install -U discord.py
+``` -->
+
+## Youtube API
+### Install dependencies
+more info [here](https://developers.google.com/youtube/v3/quickstart/python)
+
+```bash
+#Note: make sure your pip version is 3.x. Alternatively, use pip3. If you're using a pyenv, make sure it's activated
+pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+```
+### Create your credentials.json file
+more info [here](https://developers.google.com/workspace/guides/create-credentials)
+
+1. Go to https://console.cloud.google.com/ and create a new project (or use an existing one).
+1. Add `Youtube Data API v3` to your project.
+1. Go to `APIs & Services` > `OAuth consent screen`
+	1. Click `External` User Type, and fill in all of the required information
+1. Go to `APIs & Services` > `Credentials` > `Create Credentials` > `OAuth Client ID`
+	1. Fill in the required information. Once you're done, you should be brought back to the `Credentials` page.
+1. Under OAuth 2.0 Client IDs, you should now have a new Client ID. Go to the right and click download.
+1. rename the file to `credentials.json` and place it in the root directory of this reposity.
+1. Go to `APIs & Services` > `OAuth consent screen` > `Audience` > `Test Users` and add your email as a Test User.
+
+<!-- 
+### Configuring Bot Token And SpreadSheetId
+
+You need to setup 1 env_vars:
+
+`BOT_TOKEN` should be your discord bot token.
+
+Alternatively, you can create an additional files: `bottoken.txt`
+
+`bottoken.txt` should contain your discord bot token. -->
+
+
+Afterwards... simpy run :D
+```bash
+python notifier/youtubenotifier.py
+```
+
+Note: First time running will open up a tab, where you will need to allow permissions and get through Safety in accessing the spreadsheet. If you get one of those red warning insecure pages, you need to click `Advanced` > `Continue to page`.
+
+Note: When running for the first time, you might also get hit with the following
+
+`Access blocked: YoutubeNotifier has not completed the Google verification process`
+
+In which case, you forgot to add your google account as a test user to your Google project.
---
+++ notifier/youtubenotifier.py
@@ -0,0 +1,289 @@
+# Google Sheets API Imports
+from __future__ import print_function
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+import json
+
+import requests
+import time
+
+DEBUG = False
+
+# SLEEP_TIMER = 3600 # sleep for 1 hour
+SLEEP_TIMER = 300 # sleep for 15 mins
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/youtube.readonly"]
+
+
+USERNAME = "SOPD Ballers"; 
+AVATAR_URL = "https://yt3.googleusercontent.com/iITluvBkSaSTQeKScFbpE5V4CKJl7mNnrfneeLoGzEZ8YHNdsf9KjX75y9etRSb4Ibwdvb7uCJ4=s160-c-k-c0x00ffffff-no-rj";
+
+# // The URL of your Discord webhook
+# TEST_WEBHOOK_URL = "https://discord.com/api/webhooks/1358344057079988374/sQjj9_xuZm21Jsy7FfYbFt_jbCAllj92agMrsYhUhFAWjegt4lNaceUQCXCw4k3Cecxz"
+# WEBHOOK_URL = TEST_WEBHOOK_URL
+WEBHOOK_URL = "https://discord.com/api/webhooks/1356520107010166874/hv1Lf-yGguIIX8uO34HdPJ3363ILQ3_cmjMGL4fnP0pUJBNcD3bl3yNNAVSkXOlV0yjY"; 
+
+# playlists to Monitor
+CHANNEL_ID = "UCI1RCT47NuGEDxTfwiwBVLg"
+
+# Playlists subscribed
+S_MISC_PLAYLIST_ID = "PLMv_MOdIUtdLJ4S0uSKL2napYRIsQZO99"
+S_ELV_PLAYLIST_ID = "PLMv_MOdIUtdKNBx3u_V6UR4-JvG-oTlvE"
+S_AVL_PLAYLIST_ID = "PLMv_MOdIUtdLuA0ggNn7294qfH9D2VcLb"
+SCVC_PLAYLIST_ID = "PLMv_MOdIUtdKrvKlwZ0XYr2dcteU96iSv"
+SCVC_DRILLS_PLAYLIST_ID = "PLMv_MOdIUtdKRPtukJR5b3CNHxVrBUE3R"
+SOPD_PLAYLIST_ID = "PLMv_MOdIUtdIdZ6DG9r_lTIArhjMfWUdv"
+
+SUBSCRIBED_PLAYLISTS = [S_MISC_PLAYLIST_ID, S_ELV_PLAYLIST_ID, S_AVL_PLAYLIST_ID, SCVC_PLAYLIST_ID, SCVC_DRILLS_PLAYLIST_ID, SOPD_PLAYLIST_ID]
+# SUBSCRIBED_PLAYLISTS = [SOPD_PLAYLIST_ID]
+
+# Other playlists
+W_RED_TEMP_PLAYLIST_ID = "PLMv_MOdIUtdLrxxk87uogylhGKfhXfALQ"
+LA_FITNESS_PLAYLIST_ID = "PLMv_MOdIUtdKV93qAzEzb36EXUxTF5mYt"
+W_MISC_PLAYLIST_ID = "PLMv_MOdIUtdITcGzYTfaglXtYCMDAw9IU"
+W_KIRK_PLAYLIST_ID = "PLMv_MOdIUtdIX1tQTwX03JSIc-5qPRhBc"
+W_AVL_PLAYLIST_ID = "PLMv_MOdIUtdKvle61fhLGDO38kbdgc39t"
+
+DATA_FOLDER = "data"
+DATA_FILE = f"{DATA_FOLDER}/data.json"
+TEST_DATA_FILE = f"{DATA_FOLDER}/test_data.json"
+
+
+PLAYLIST_URL = "https://www.youtube.com/playlist?list="
+YOUTUBE_SHORT_URL = "https://youtu.be/"
+
+def GetPlaylistURL(playlistId):
+    return f"{PLAYLIST_URL}{playlistId}"
+
+# https://youtu.be/XpFzdrr_B3s?list=PLMv_MOdIUtdLJ4S0uSKL2napYRIsQZO99
+def GetVideoURLWithPlaylist(videoId, playlistId):
+    return f"{YOUTUBE_SHORT_URL}{videoId}?list={playlistId}"
+
+class Database():
+    def add(self, data):
+        playlist = {
+            "id":data["id"],
+            "title": data["title"],
+            "numVideos": data["numVideos"]
+            }
+        
+        self.playlists.append(playlist)
+    
+    def updated(data):
+        pass
+        
+class Youtube:    
+    def __init__(self):
+        self.service = self.setupYoutubeAPI()
+
+    def setupYoutubeAPI(self):
+        print("Setting up connection to Youtube API...")
+
+        api_service_name = "youtube"
+        api_version = "v3"
+        
+        """Shows basic usage of the Sheets API.
+        Prints values from a sample spreadsheet.
+        """
+        creds = None
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.json'):
+            creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
+
+        service = build(api_service_name, api_version, credentials=creds)
+        return service
+
+    # unused
+    def fetchChannelPlaylists(self, channelId):
+        request = self.service.playlists().list(
+            part="contentDetails,id,snippet",
+            channelId=channelId,
+            maxResults=50, # TODO: once there's more than 50 playlists, we need to update this code to use `nextPageToken`
+        )
+        response = request.execute()
+
+        channel = {"channelId ": channelId,
+                   "playlists": [
+                       {"id":playlist["id"],
+                        "title": playlist["snippet"]["title"],
+                        "numVideos": playlist["contentDetails"]["itemCount"] } for playlist in response["items"]]
+        }
+        
+        with open(DATA_FILE, 'w') as file:
+            json.dump(channel, file, indent=2)
+
+    def fetchPlaylist(self, playlistId):
+        request = self.service.playlistItems().list(
+            part="contentDetails, id, snippet, status",
+            maxResults=50,
+            playlistId=playlistId
+        )
+        response = request.execute()
+        numResults = response["pageInfo"]["totalResults"]
+        # print(response)
+        
+        videos= {}
+        for video in response["items"]:
+            videos[video["id"]] = {"title": video["snippet"]["title"],
+                                   "videoId": video["contentDetails"]["videoId"],
+                                   "playlistId": playlistId,
+                                   "uploaded": True if video["snippet"]["thumbnails"].get("maxres") else False}
+        
+        while "nextPageToken" in response:
+            request = self.service.playlistItems().list(
+                part="contentDetails, id, snippet",
+                maxResults=50,
+                playlistId=playlistId,
+                pageToken=response["nextPageToken"]
+                )
+            response = request.execute()
+            for video in response["items"]:
+                videos[video["id"]] = {"title": video["snippet"]["title"],
+                                       "videoId": video["contentDetails"]["videoId"],
+                                       "playlistId": playlistId,
+                                       "uploaded": True if video["snippet"]["thumbnails"].get("maxres") else False}
+            # print(response)
+        return numResults, videos
+
+def loadData():
+    with open(DATA_FILE, 'r') as file:
+        return json.load(file)
+    
+def saveData(data):
+    with open(DATA_FILE, 'w') as file:
+        json.dump(data, file, indent=2)
+    
+def loadTestData():
+    with open(TEST_DATA_FILE, 'r') as file:
+        return json.load(file)
+
+def postVideoUpdateToDiscord(video):
+    url = WEBHOOK_URL
+    headers = {
+        "Content-type": "application/json",
+    }
+    data = {
+        "username": USERNAME,
+        "avatar_url": AVATAR_URL,
+        "content": f"Hey Ballers, New Video just dropped!\n    **{video.get('title')}**: {GetVideoURLWithPlaylist(video.get('videoId'), video.get('playlistId'))}",
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    print(response)
+
+def postUpdateToDiscord(videos):
+    
+    numVideos = len(videos)
+    if(numVideos == 0):
+        return
+    
+    if(numVideos == 1):
+        postVideoUpdateToDiscord(videos[0])
+        return
+        
+    
+    url = WEBHOOK_URL
+    headers = {
+        "Content-type": "application/json",
+    }
+    
+    content = f"Hey Ballers, We got {numVideos} New Videos for y'all!"
+    for video in videos:
+        content += f"\n    **{video.get('title')}**: {GetVideoURLWithPlaylist(video.get('videoId'), video.get('playlistId'))}"
+    data = {
+        "username": USERNAME,
+        "avatar_url": AVATAR_URL,
+        "content": content,
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    print(response)
+    
+def postNewPlaylistToDiscord(playlistId):
+    url = WEBHOOK_URL
+    headers = {
+        "Content-type": "application/json",
+    }
+    data = {
+        "username": USERNAME,
+        "avatar_url": AVATAR_URL,
+        "content": f"Hey Ballers, New Playlist just dropped!\n    {GetPlaylistURL(playlistId)}",
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    print(response)
+
+youtube = Youtube()
+oldData = loadData()
+# oldData = loadTestData()
+while True:
+# if True:
+    updated = False
+    newData = {}
+    for playlistId in SUBSCRIBED_PLAYLISTS:
+        numVideos, videos = youtube.fetchPlaylist(playlistId)
+        newData[playlistId] = {"count":numVideos, "videos":videos}
+    
+    # saveData(newData)
+    for playlistId in SUBSCRIBED_PLAYLISTS:
+        oldPlaylist = oldData.get(playlistId)
+        newPlaylist = newData.get(playlistId)
+        
+        if oldPlaylist is None:
+            # New playlist
+            print(playlistId, "New Playlist")
+            updated = True
+            postNewPlaylistToDiscord(playlistId)
+        else:
+            newVideos = []
+            videosUploading = []
+            for key, value in newPlaylist["videos"].items():
+                if key not in oldPlaylist["videos"].keys(): # new video
+                    if not value["uploaded"]: # not finished uploading.
+                        videosUploading.append(value)
+                    else:
+                        newVideos.append(value)
+                elif not oldPlaylist["videos"][key]["uploaded"]: # previously uploading video
+                    if value["uploaded"]: #done uploading
+                        # print(f"adding {value} since {key} done uploading")
+                        newVideos.append(value)
+                    else: # still uploading
+                        videosUploading.append(value)
+        
+            if videosUploading != []:
+                # videos are still uploading
+                print(playlistId, f"{len(videosUploading)} video(s) are still uploading...\t{len(newVideos)} video(s) ready to report...")
+                continue
+            
+            if newVideos != []:
+                print(playlistId, f"{len(newVideos)} videos to update")
+                postUpdateToDiscord(newVideos)
+                updated = True
+            else:
+                print(playlistId, "No videos to update")
+    
+    if updated:
+        saveData(newData)
+        oldData = loadData()
+        updated = False
+
+    print(f"sleeping for {SLEEP_TIMER} seconds...\n")
+    time.sleep(SLEEP_TIMER)
